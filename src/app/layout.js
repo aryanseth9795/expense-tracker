afad32aa13a877940e921c0c4de410214798645c
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2025-07-05 19:10:12 +0530

    Nextjs setup for expense tracker

---
+++ src/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono,Roboto_Slab } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Roboto_Slab({
+  variable: "--font-roboto-slab",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Expense Tracker",
+  description: "Track your expenses efficiently",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
